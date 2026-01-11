36dcba487807ed4370b397034661cb8fa48e78b4
Azhe Kun <root@corp.azhe.my.id>
2026-01-12 00:46:00 +0700

    fix dotnet app

--- apps/dotnet-svc/src/Domain/Entities/WeatherForecast.cs
+++ apps/dotnet-svc/src/Domain/Entities/WeatherForecast.cs
@@ -6,5 +6,12 @@ public class WeatherForecast
     public int TemperatureC { get; set; }
     public string? Summary { get; set; }
     
+    public WeatherForecast(DateOnly date, int temperatureC, string? summary)
+    {
+        Date = date;
+        TemperatureC = temperatureC;
+        Summary = summary;
+    }
+    
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
